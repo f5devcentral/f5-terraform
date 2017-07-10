783f8f4c264c13a029d641cd8d8124c612c543fa
Alex Applebaum <a.applebaum@f5.com>
2017-07-10 15:57:26 -0700

    added 2nic & 3nic modules

---
+++ modules/providers/aws/infrastructure/proxy/standalone/2nic/util/user_data_basic_asg_pool.tpl
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+# Script must be non-blocking or run in the background.
+
+mkdir -p /config/cloud
+
+cat << 'EOF' > /config/cloud/startup-script.sh
+
+
+#!/bin/bash
+
+## 2NIC BIG-IP ONBOARD SCRIPT
+
+## IF THIS SCRIPT IS LAUNCHED EARLY IN BOOT (ex. when from cloud-init), YOU NEED TO RUN IT IN THE BACKGROUND TO NOT BLOCK OTHER STARTUP FUNCTIONS
+# ex. location of interpolated cloud-init script
+#/opt/cloud/instances/i-079ac8a174eb1727a/scripts/part-001
+
+LOG_FILE=/var/log/startup-script.log
+if [ ! -e $LOG_FILE ]
+then
+     touch $LOG_FILE
+     exec &>>$LOG_FILE
+     # nohup $0 0<&- &>/dev/null &
+else
+    #if file exists, exit as only want to run once
+    exit
+fi
+
+
+### ONBOARD INPUT PARAMS 
+
+region=${region}
+
+adminUsername='${admin_username}'
+adminPassword='${admin_password}'
+
+hostname=`curl --silent --fail --retry 20 http://169.254.169.254/latest/meta-data/hostname`
+dnsServer=${dns_server}
+ntpServer=${ntp_server}
+timezone=${timezone}
+
+# Management Interface uses DHCP 
+# v13 uses mgmt for ifconfig & defaults to 8443 for GUI for Single Nic Deployments
+if ifconfig mgmt; then managementInterface=mgmt; else managementInterface=eth0; fi
+managementAddress=$(egrep -m 1 -A 1 $managementInterface /var/lib/dhclient/dhclient.leases | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+managementGuiPort=${management_gui_port}
+
+# Network
+subnet1Name=${subnet_1_name}
+subnet1Address=${subnet_1_address}
+subnet1CidrBlock=${subnet_1_cidr_block}
+
+subnet1Network=$${subnet1CidrBlock%/*}
+subnet1Prefix=$${subnet1CidrBlock#*/}
+subnet1Mtu=${subnet_1_mtu}
+
+defaultGateway=${default_gateway}
+
+
+### DOWNLOAD ONBOARDING LIBS
+# Could be pre-packaged or hosted internally
+
+libs_dir="/config/cloud/aws/node_modules"
+mkdir -p $libs_dir
+curl -o /config/cloud/f5-cloud-libs.tar.gz --silent --fail --retry 60 -L https://raw.githubusercontent.com/F5Networks/f5-cloud-libs/v3.1.1/dist/f5-cloud-libs.tar.gz
+curl -o /config/cloud/f5-cloud-libs-aws.tar.gz --silent --fail --retry 60 -L https://raw.githubusercontent.com/F5Networks/f5-cloud-libs-aws/v1.3.0/dist/f5-cloud-libs-aws.tar.gz
+tar xvfz /config/cloud/f5-cloud-libs.tar.gz -C $libs_dir
+tar xvfz /config/cloud/f5-cloud-libs-aws.tar.gz -C $libs_dir/f5-cloud-libs/node_modules
+
+
+### BEGIN BASIC ONBOARDING 
+
+# WAIT FOR MCPD (DATABASE) TO BE UP TO BEGIN F5 CONFIG
+
+. $libs_dir/f5-cloud-libs/scripts/util.sh
+wait_for_bigip
+
+# PASSWORD
+# Generate Random Password
+#f5-rest-node $libs_dir/f5-cloud-libs/scripts/generatePassword --file /config/cloud/aws/.adminPassword"
+#adminPassword=$(/bin/sed -e $'s:[!\\'\"%{};/|#\\x20\\\\\\\\]:\\\\\\\\&:g' < /config/cloud/aws/.adminPassword)      
+# Use Password Provided as Input Param
+tmsh create auth user $${adminUsername} password $${adminPassword} shell bash partition-access replace-all-with { all-partitions { role admin } }
+tmsh save /sys config
+
+# License / Provision
+f5-rest-node $libs_dir/f5-cloud-libs/scripts/onboard.js \
+-o  /var/log/onboard.log \
+--no-reboot \
+--port $${managementGuiPort} \
+--ssl-port $${managementGuiPort} \
+--host localhost \
+--user $${adminUsername} \
+--password $${adminPassword} \
+--hostname $${hostname} \
+--global-setting hostname:$${hostname} \
+--dns $${dnsServer} \
+--ntp $${ntpServer} \
+--tz $${timezone} \
+--module ltm:nominal \
+--ping www.f5.com 30 15 \ 
+
+# Network
+
+f5-rest-node $libs_dir/f5-cloud-libs/scripts/network.js \
+-o /var/log/onboard-network.log \
+--host localhost \
+--user $${adminUsername} \
+--password $${adminPassword} \
+--local-only \
+--vlan name:$${subnet1Name},nic:1.1,mtu:$${subnet1Mtu} \
+--self-ip name:$${subnet1Name}_self,address:$${subnet1Address}/$${subnet1Prefix},vlan:$${subnet1Name},'allow:tcp:443 tcp:4353 ' \
+--default-gw $${defaultGateway} \
+
+# Ex of adding additional ports to Self-IP Allow List - need to quote
+# --self-ip name:$${subnet1Name}_self,address:$${subnet1Address}/$${subnet1Prefix},vlan:$${subnet1Name},'allow:tcp:443 tcp:80 tcp:4353' \
+
+
+############ BEGIN CUSTOM CONFIG ############
+
+# SOME HIGH LEVEL CONFIG PARAMS
+
+region=${region}
+
+applicationName=${application}
+virtualServiceDns=${vs_dns_name}
+virtualServiceAddress=${vs_address}
+virtualServiceMask=${vs_mask}
+virtualServicePort=${vs_port}
+
+applicationPort=${pool_member_port}
+applicationPoolName=${pool_name}
+applicationPoolTagKey=${pool_tag_key}
+applicationPoolTagValue=${pool_tag_value}
+
+
+# DOWNLOAD SOME FILES
+curl --silent --fail --retry 20 -o /config/cloud/f5.http.v1.2.0.tmpl https://raw.githubusercontent.com/f5devcentral/f5-cloud-init-examples/master/files/iApp/f5.http.v1.2.0.tmpl
+curl --silent --fail --retry 20 -o /config/cloud/appsvcs_integration_v2.1_001.tmpl https://raw.githubusercontent.com/f5devcentral/f5-cloud-init-examples/master/files/iApp/appsvcs_integration_v2.1_001.tmpl
+curl --silent --fail --retry 20 -o /config/cloud/f5.service_discovery.tmpl https://raw.githubusercontent.com/f5devcentral/f5-cloud-init-examples/master/files/iApp/f5.service_discovery.tmpl
+curl --silent --fail --retry 20 -o /config/cloud/f5.analytics.tmpl https://raw.githubusercontent.com/f5devcentral/f5-cloud-init-examples/master/files/iApp/f5.analytics.tmpl
+
+# Load iApps
+tmsh load sys application template /config/cloud/f5.http.v1.2.0.tmpl
+tmsh load sys application template /config/cloud/appsvcs_integration_v2.1_001.tmpl
+tmsh load sys application template /config/cloud/f5.service_discovery.tmpl
+tmsh load sys application template /config/cloud/f5.analytics.tmpl
+
+
+# CREATE SSL PROFILES
+tmsh install sys crypto cert site.example.com from-local-file /config/ssl/ssl.crt/default.crt
+tmsh install sys crypto key site.example.com from-local-file /config/ssl/ssl.key/default.key
+
+
+# SERVICE DISCOVERY
+
+# POOL = ASG
+tmsh create ltm pool $${applicationPoolName} monitor http
+
+tmsh create sys application service $${applicationName}_sd { template f5.service_discovery variables add { basic__advanced { value no } basic__display_help { value hide } cloud__aws_region { value $${region} } cloud__aws_use_role { value no } cloud__cloud_provider { value aws } pool__interval { value 60 } pool__lb_method_choice { value least-connections-member } pool__member_conn_limit { value 0 } pool__member_port { value $${applicationPort} } pool__pool_to_use { value /Common/$${applicationPoolName} } pool__public_private { value private } pool__tag_key { value $${applicationPoolTagKey} } pool__tag_value { value $${applicationPoolTagValue} } }}
+
+# SERVICE INSERTION: CREATE VIRTUAL
+tmsh create sys application service $${applicationName} { template f5.http.v1.2.0 tables add { pool__hosts { column-names { name } rows { { row { $${virtualServiceDns} } } } } pool__members { column-names { addr port connection_limit } rows {{ row { $${applicationName} $${applicationPort} 0 }}}}} variables add { pool__addr { value $${virtualServiceAddress} } pool__mask { value $${virtualServiceMask} } pool__port { value $${virtualServicePort} } pool__port_secure { value $${virtualServicePort} } net__vlan_mode { value all } ssl__cert { value /Common/site.example.com.crt } ssl__key { value /Common/site.example.com.key } ssl__mode { value client_ssl } ssl_encryption_questions__advanced { value yes } ssl_encryption_questions__help { value hide } monitor__http_version { value http11 } pool__pool_to_use { value /Common/$${applicationPoolName} } }}
+
+
+# WARNING: If creating a user via startup script, remember to change the password as soon as you login or dispose after provisioning.
+# tmsh delete auth user $${adminUsername}
+
+############ END CUSTOM HIGH CONFIG ############
+
+tmsh save /sys config
+date
+echo "FINISHED STARTUP SCRIPT"
+
+
+EOF
+
+
+# Now run in the background to not block startup
+chmod 755 /config/cloud/startup-script.sh 
+nohup /config/cloud/startup-script.sh &
+
+
---
+++ modules/providers/aws/infrastructure/proxy/standalone/3nic/util/bigip.tf
@@ -0,0 +1,419 @@
+# CREATES 3NIC BIG-IP
+
+### VARIABLES ###
+
+# TAGS
+variable purpose        { default = "public"       }  
+variable environment    { default = "dev"          }  
+variable application    { default = "f5app"        }  
+variable owner          { default = "f5owner"      }  
+variable group          { default = "f5group"      } 
+variable costcenter     { default = "f5costcenter" } 
+
+
+# NETWORK:
+variable region                 { default = "us-west-2"  }
+variable availability_zone      { default = "us-west-2a" }
+variable vpc_id                 {}
+variable management_subnet_id   {}
+variable management_address     { default = "10.0.0.11"   }
+variable subnet_1_id            {}
+variable subnet_1_name          { default = "public"      }
+variable subnet_1_address       { default = "10.0.1.11"   }
+variable subnet_1_cidr_block    { default = "10.0.1.0/24" }
+variable subnet_1_mtu           { default = "1500"        }
+variable subnet_2_id            {}
+variable subnet_2_name          { default = "private"      }
+variable subnet_2_address       { default = "10.0.2.11"   }
+variable subnet_2_cidr_block    { default = "10.0.2.0/24" }
+variable subnet_2_mtu           { default = "1500"        }
+
+variable default_gateway        { default = "10.0.1.1"    }
+
+
+# Public IPs. a Public NAT
+# NOTE: either NAT gateway or other NAT service is required for BIG-IP to operate
+# It needs internet access through the MGMT interface & Interface with Default Gateway
+# which is ususally subnet_1 (aka "Public" or "External" interface)
+variable create_management_public_ip  { default = false         }
+variable create_subnet_1_public_ip    { default = true          }
+variable create_vs_public_ip          { default = true          }
+
+
+# SYSTEM
+variable dns_server           { default = "8.8.8.8" }
+variable ntp_server           { default = "0.us.pool.ntp.org" }
+variable timezone             { default = "UTC" }
+variable management_gui_port  { default = "443" }
+
+# PROXY:
+variable instance_type  { default = "m4.2xlarge" }
+variable amis { 
+    type = "map" 
+    default = {
+        "ap-northeast-1" = "ami-3b1e2f5c"
+        "ap-northeast-2" = "ami-e0dc018e"
+        "ap-southeast-1" = "ami-530eb430"
+        "ap-southeast-2" = "ami-60d8d303"
+        "eu-central-1"   = "ami-c24e91ad"
+        "eu-west-1"      = "ami-1fbdb079"
+        "sa-east-1"      = "ami-d58de1b9"
+        "us-east-1"      = "ami-09721c1f"
+        "us-east-2"      = "ami-3c183f59"
+        "us-west-1"      = "ami-c46f49a4"
+        "us-west-2"      = "ami-6bbd260b"
+    }
+}
+
+
+# SECURITY
+variable admin_username {}
+variable admin_password {}
+
+variable ssh_key_name        {}  # example "my-terraform-key"
+variable restricted_src_address { default = "0.0.0.0/0" }
+
+# NOTE certs not used below but keeping as optional input in case need to extend
+variable site_ssl_cert  { default = "not-required-if-terminated-on-lb" }
+variable site_ssl_key   { default = "not-required-if-terminated-on-lb" }
+
+
+# APPLICATION
+variable vs_dns_name      { default = "www.example.com" }
+variable vs_address       { default = "10.0.1.100" }  #do not use 0.0.0.0 as used to to provision secondary IP address on public nic
+variable vs_mask          { default = "255.255.255.255" }
+variable vs_port          { default = "443" }
+
+# SERVICE DISCOVERY
+variable pool_member_port { default = "80" }
+variable pool_name        { default = "www.example.com" }  # DNS (ex. "www.example.com") used to create fqdn node if there's no Service Discovery iApp 
+variable pool_tag_key     { default = "Name" }
+variable pool_tag_value   { default = "dev-www-instance" }
+
+
+
+### RESOURCES ###
+
+resource "aws_security_group" "public_int_sg" {
+  name        = "${var.environment}-proxy-public-int-sg"
+  description = "${var.environment}-proxy-public-int-ports"
+  vpc_id      = "${var.vpc_id}"
+
+  # VIP HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # VIP HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Sync or GTM Discovery access from anywhere
+  ingress {
+    from_port   = 4353
+    to_port     = 4353
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # ping access from internal
+  ingress {
+    from_port   = 8 
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+      Name           = "${var.environment}-proxy-public-int-sg"
+      environment    = "${var.environment}"
+      owner          = "${var.owner}"
+      group          = "${var.group}"
+      costcenter     = "${var.costcenter}"
+      application    = "${var.application}"
+  }
+
+}
+
+resource "aws_security_group" "management_int_sg" {
+  name        = "${var.environment}-proxy-management-int-sg"
+  description = "${var.environment}-proxy-management-int-ports"
+  vpc_id      = "${var.vpc_id}"
+
+  # MGMT ssh access 
+  ingress {
+    from_port   = 22 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+
+  # MGMT HTTPS access 
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+
+  # ping access 
+  ingress {
+    from_port   = 8 
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+      Name           = "${var.environment}-proxy-management-int-sg"
+      environment    = "${var.environment}"
+      owner          = "${var.owner}"
+      group          = "${var.group}"
+      costcenter     = "${var.costcenter}"
+      application    = "${var.application}"
+  }
+
+}
+
+resource "aws_security_group" "private_int_sg" {
+  name        = "${var.environment}-proxy-private-int-sg"
+  description = "${var.environment}-proxy-private-int-ports"
+  vpc_id      = "${var.vpc_id}"
+
+  # MGMT ssh access 
+  ingress {
+    from_port   = 22 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+
+  # MGMT HTTPS access 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+
+  # ping access 
+  ingress {
+    from_port   = 8 
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["${var.restricted_src_address}"]
+  }
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+      Name           = "${var.environment}-proxy-private-int-sg"
+      environment    = "${var.environment}"
+      owner          = "${var.owner}"
+      group          = "${var.group}"
+      costcenter     = "${var.costcenter}"
+      application    = "${var.application}"
+  }
+
+}
+
+
+resource "aws_network_interface" "public_int" {
+    subnet_id = "${var.subnet_1_id}"
+    private_ips = ["${var.subnet_1_address}","${var.vs_address}"]
+    security_groups = ["${aws_security_group.public_int_sg.id}"]
+    attachment {
+        instance = "${aws_instance.bigip.id}"
+        device_index = 1
+    }
+}
+
+resource "aws_network_interface" "private_int" {
+    depends_on = ["aws_network_interface.public_int"]
+    subnet_id = "${var.subnet_2_id}"
+    private_ips = ["${var.subnet_2_address}"]
+    security_groups = ["${aws_security_group.private_int_sg.id}"]
+    attachment {
+        instance = "${aws_instance.bigip.id}"
+        device_index = 2
+    }
+}
+
+
+resource "aws_eip" "public_self_eip" {
+  count                     = "${var.create_subnet_1_public_ip}"
+  vpc                       = true
+  network_interface         = "${aws_network_interface.public_int.id}"
+  associate_with_private_ip = "${var.subnet_1_address}"
+}
+
+resource "aws_eip" "virtual_service_eip" {
+  count                     = "${var.create_vs_public_ip}"
+  vpc                       = true
+  network_interface         = "${aws_network_interface.public_int.id}"
+  associate_with_private_ip = "${var.vs_address}"
+}
+
+
+resource "aws_iam_role_policy" "proxy_service_discovery_policy" {
+  name = "proxy-service-discovery-policy"
+  role = "${aws_iam_role.proxy_service_discovery_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeAddresses",
+        "ec2:AssociateAddress",
+        "ec2:DisassociateAddress",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DescribeNetworkInterfaceAttributes",
+        "ec2:DescribeRouteTables",
+        "ec2:ReplaceRoute",
+        "autoscaling:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "proxy_service_discovery_role" {
+  name = "proxy-service-discovery-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_instance_profile" "proxy_service_discovery_profile" {
+  name  = "proxy-service-discovery-profile"
+  role = "${aws_iam_role.proxy_service_discovery_role.name}"
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user_data.tpl")}"
+
+  vars {
+    admin_username        = "${var.admin_username}"
+    admin_password        = "${var.admin_password}"
+    management_gui_port   = "${var.management_gui_port}"
+    dns_server            = "${var.dns_server}"
+    ntp_server            = "${var.ntp_server}"
+    timezone              = "${var.timezone}"
+    subnet_1_name         = "${var.subnet_1_name}"
+    subnet_1_address      = "${var.subnet_1_address}" 
+    subnet_1_cidr_block   = "${var.subnet_1_cidr_block}"
+    subnet_1_mtu          = "${var.subnet_1_mtu}"
+    subnet_2_name         = "${var.subnet_2_name}"
+    subnet_2_address      = "${var.subnet_2_address}" 
+    subnet_2_cidr_block   = "${var.subnet_2_cidr_block}"
+    subnet_2_mtu          = "${var.subnet_2_mtu}" 
+    default_gateway       = "${var.default_gateway}" 
+    application           = "${var.application}"
+    region                = "${var.region}"
+    vs_dns_name           = "${var.vs_dns_name}"
+    vs_address            = "${var.vs_address}"
+    vs_mask               = "${var.vs_mask}"
+    vs_port               = "${var.vs_port}"
+    pool_member_port      = "${var.pool_member_port}"
+    pool_name             = "${var.pool_name}"
+    pool_tag_key          = "${var.pool_tag_key}"
+    pool_tag_value        = "${var.pool_tag_value}"
+    site_ssl_cert         = "${var.site_ssl_cert}"
+    site_ssl_key          = "${var.site_ssl_key}"
+  }
+}
+
+resource "aws_instance" "bigip" {
+    ami = "${lookup(var.amis, var.region)}"
+    instance_type = "${var.instance_type}"
+    associate_public_ip_address = "${var.create_management_public_ip}"
+    private_ip = "${var.management_address}" 
+    availability_zone = "${var.availability_zone}"
+    subnet_id = "${var.management_subnet_id}"
+    vpc_security_group_ids = ["${aws_security_group.management_int_sg.id}"]
+    iam_instance_profile = "${aws_iam_instance_profile.proxy_service_discovery_profile.name}"
+    key_name = "${var.ssh_key_name}"
+    root_block_device { delete_on_termination = true }
+    tags {
+      Name           = "${var.environment}-proxy"
+      environment    = "${var.environment}"
+      owner          = "${var.owner}"
+      group          = "${var.group}"
+      costcenter     = "${var.costcenter}"
+      application    = "${var.application}"
+    }
+    user_data = "${data.template_file.user_data.rendered}"
+}
+
+
+
+### OUTPUTS ###
+
+output "instance_id" { value = "${aws_instance.bigip.id}"  }
+
+output "management_int_sg_id" { value = "${aws_security_group.management_int_sg.id}" }
+output "management_int_sg_name" { value = "${aws_security_group.management_int_sg.name}" }
+
+output "public_int_sg_id" { value = "${aws_security_group.public_int_sg.id}" }
+output "public_int_sg_name" { value = "${aws_security_group.public_int_sg.name}" }
+
+output "private_int_sg_id" { value = "${aws_security_group.private_int_sg.id}" }
+output "private_int_sg_name" { value = "${aws_security_group.private_int_sg.name}" }
+
+output "management_int_private_ip" { value = "${aws_instance.bigip.private_ip}" }
+output "management_int_public_ip" { value = "${aws_instance.bigip.public_ip}" }
+
+output "public_int_public_ip" { value = "${aws_eip.public_self_eip.public_ip}" }
+output "public_int_private_ip" { value = "${aws_network_interface.public_int.private_ips[0]}" }
+
+output "private_int_private_ip" { value = "${aws_network_interface.private_int.private_ips[0]}" }
+
+output "virtual_service_public_ip" { value = "${aws_eip.virtual_service_eip.public_ip}" }
+
+
